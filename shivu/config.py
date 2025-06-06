d6cf785374859c3f3d04044345ea68a06ba445a4
Sajidedix <sajideditz27@gmail.com>
2025-06-06 16:07:38 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,10 +5,10 @@ class Config(object):
     OWNER_ID = "7657218453"
     sudo_users = ["7657218453", "7135097212", "6957310269"]
     GROUP_ID = "-1002746226173"
-    TOKEN = "your bot token"
-    mongo_url = "your db"
+    TOKEN = "7517205575:AAHJgmhzzI0sRy5RDddvcuihdLaSwmEa-6U"
+    mongo_url = "mongodb+srv://chffdgf0:olpYdyMsXzEGXqS5@cluster0.ivpoxxi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://envs.sh/gOG.jpg", "https://envs.sh/gOK.jpg", "https://envs.sh/gOz.jpg", "https://envs.sh/gO3.jpg"]
-    SUPPORT_CHAT = "lustsupport"
+    SUPPORT_CHAT = "SandVillage"
     UPDATE_CHAT = "lustxUpdate"
     BOT_USERNAME = "lustXcatcherrobot"
     CHARA_CHANNEL_ID = "-1002592320932"
